fab27c2e371b21bcfb3b656f1aefed36044da523
Ian Lesikar and Ryan Carlile <ian@>
2016-04-26 09:41:49 -0700

    add css styling and reformatting of index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,32 +10,32 @@ public class App {
   public static void main(String[] args) {
 
 
-    // staticFileLocation("/public");
+    staticFileLocation("/public");
 
-      // get("/", (request, response) -> {
-      //   Map<String, Object> model = new HashMap<String, Object>();
-      //   model.put("places", request.session().attribute("places"));
-      //   model.put("template", "templates/index.vtl");
-      //   return new ModelAndView(model, "templates/layout.vtl");
-      // }, new VelocityTemplateEngine());
-      //
-      // post("/places", (request, response) -> {
-      //   Map<String, Object> model = new HashMap<String, Object>();
-      //
-      //   ArrayList<Place> places = request.session().attribute("places");
-      //   if (places == null) {
-      //     places = new ArrayList<Place>();
-      //     request.session().attribute("places", places);
-      //   }
-      //
-      //   String description = request.queryParams("description");
-      //   Place newPlace = new Place(description);
-      //   places.add(newPlace);
-      //   // request.session().attribute("places",newPlace);
-      //
-      //   model.put("template", "templates/success.vtl");
-      //   return new ModelAndView(model, "templates/layout.vtl");
-      // }, new VelocityTemplateEngine());
+      get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("places", request.session().attribute("places"));
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
+
+      post("/places", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        ArrayList<Places> places = request.session().attribute("places");
+        if (places == null) {
+          places = new ArrayList<Places>();
+          request.session().attribute("places", places);
+        }
+
+        String description = request.queryParams("description");
+        Places newPlace = new Places(description);
+        places.add(newPlace);
+        // request.session().attribute("places",newPlace);
+
+        model.put("template", "templates/success.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
   }
 
 
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,55 +1,55 @@
-// import org.fluentlenium.adapter.FluentTest;
-// import org.junit.ClassRule;
-// import org.junit.Test;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-// import static org.fluentlenium.core.filter.FilterConstructor.*;
-// import java.util.ArrayList;
-//
-// import static org.assertj.core.api.Assertions.assertThat;
-//
-// public class AppTest extends FluentTest {
-//   public WebDriver webDriver = new HtmlUnitDriver();
-//
-//   @Override
-//   public WebDriver getDefaultDriver() {
-//     return webDriver;
-//   }
-//
-//   @ClassRule
-//   public static ServerRule server = new ServerRule();
-//
-//   @Test
-//   public void rootTest() {
-//     goTo("http://localhost:4567/");
-//     assertThat(pageSource()).contains("Place list!");
-//   }
-//
-//   @Test
-//   public void placeIsCreatedTest() {
-//     goTo("http://localhost:4567/");
-//     fill("#description").with("Mow the lawn");
-//     submit(".btn");
-//     assertThat(pageSource()).contains("Your place has been saved.");
-//   }
-//   @Test
-//   public void placeIsDisplayedTest() {
-//     goTo("http://localhost:4567/");
-//     fill("#description").with("Mow the lawn");
-//     submit(".btn");
-//     click("a", withText("Go Back"));
-//     assertThat(pageSource()).contains("Mow the lawn");
-//   }
-//   @Test
-//   public void multiplePlacesAreDisplayedTest() {
-//     goTo("http://localhost:4567/");
-//     fill("#description").with("Mow the lawn");
-//     submit(".btn");
-//     click("a", withText("Go Back"));
-//     fill("#description").with("Buy groceries");
-//     submit(".btn");
-//     click("a", withText("Go Back"));
-//     assertThat(pageSource()).contains("Mow the lawn");
-//     assertThat(pageSource()).contains("Buy groceries");
-//   }
-// }
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Places You've Been");
+  }
+
+  @Test
+  public void placeIsCreatedTest() {
+    goTo("http://localhost:4567/");
+    fill("#description").with("Portland, OR");
+    submit(".btn");
+    assertThat(pageSource()).contains("Your place has been saved.");
+  }
+  @Test
+  public void placeIsDisplayedTest() {
+    goTo("http://localhost:4567/");
+    fill("#description").with("Portland, OR");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    assertThat(pageSource()).contains("Portland, OR");
+  }
+  @Test
+  public void multiplePlacesAreDisplayedTest() {
+    goTo("http://localhost:4567/");
+    fill("#description").with("Portland, OR");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    fill("#description").with("Seattle, WA");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    assertThat(pageSource()).contains("Portland, OR");
+    assertThat(pageSource()).contains("Seattle, WA");
+  }
+}
